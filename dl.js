75447b59fb8450b50ba12d91f7b84f1d1d7d4c01
Bill Mill <bill@billmill.org>
2024-02-24 11:17:27 -0500

    dl works a bit

---
+++ dl.js
@@ -0,0 +1,158 @@
+#!/usr/bin/env node
+
+import fs from "node:fs";
+
+import minimist from "minimist";
+import puppeteer from "puppeteer";
+import { parseActivities } from "./parseActivities.js";
+
+/**
+ * Print usage information
+ */
+function usage() {
+  console.log(`Usage: ./dl.js [<options>] [<url>]
+
+Log in to readme.com, then run a lighthouse test against <url> and output the
+report as HTML and JSON to the output directory.
+
+OPTIONS
+
+--headless: do not draw the browser window
+--help:     display this text
+`);
+}
+
+/** return a unix timestamp for a string of format "yyyy-mm-dd"
+ * @params { string } dateString
+ * @returns { int } */
+function dateToUnixTimestamp(dateString) {
+  return new Date(dateString).getTime() / 1000;
+}
+
+/** return a fetch request for all activities between dates d1 and d2, date
+/* strings formatted like "yyyy-mm-dd"
+ * @params { string } d1
+ * @params { string } d2
+ * @returns { Promise<Response> }
+ * */
+async function fetchActivities(start, end, cookie) {
+  start = dateToUnixTimestamp(start);
+  end = dateToUnixTimestamp(end);
+  const url = `https://runalyze.com/call/call.DataBrowser.display.php?start=${start}&end=${end}`;
+  console.log("fetching: ", url);
+  return fetch(url, {
+    headers: {
+      Cookie: cookie,
+      accept: "text/html, */*; q=0.01",
+      "accept-language": "en-US,en;q=0.9",
+      "sec-ch-ua": '"Chromium";v="115", "Not/A)Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-requested-with": "XMLHttpRequest",
+    },
+    referrer: "https://runalyze.com/dashboard",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+  });
+}
+
+/**
+ * Log in to runalyze.com
+ *
+ * @param {puppeteer.Page} page
+ * @param {string} origin
+ */
+async function login(page, origin) {
+  await page.goto(origin);
+  await page.waitForSelector("input#username", { visible: true });
+
+  // Fill in and submit login form.
+  const emailInput = await page.$("input#username");
+  await emailInput.type(process.env.RUNALYZE_USER);
+  const passwordInput = await page.$('input[type="password"]');
+  await passwordInput.type(process.env.RUNALYZE_PASS);
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "load" }),
+    page.click('input[type="submit"]'),
+  ]);
+}
+
+function sleep(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+
+function exists(f) {
+  try {
+    fs.statSync(f);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Log in to runalyze.com
+ *
+ * @param { string } cookie
+ */
+async function getActivities(cookie) {
+  // TODO: check if the 2024 file is fresh and download it if it's >12 hours
+  // old or summat like that
+
+  for (let year = 2023; year > 2015; year--) {
+    const fname = `./activities-${year}.json`;
+    if (!exists(fname)) {
+      const body = await (
+        await fetchActivities(`${year}-01-01`, `${year}-12-31`, cookie)
+      ).text();
+      const activities = parseActivities(body);
+      fs.writeFileSync(fname, JSON.stringify(activities));
+    }
+  }
+}
+
+async function main() {
+  // Check for presence of required environment variables
+  ["RUNALYZE_USER", "RUNALYZE_PASS"].forEach((envVar) => {
+    if (!process.env[envVar])
+      throw new Error(`${envVar} environment variable must be set`);
+  });
+
+  // parse the command line arguments with minimist:
+  // https://github.com/minimistjs/minimist#example
+  const argv = minimist(process.argv.slice(2));
+
+  // if the help flag is present, just print usage and quit
+  if (argv.help) {
+    return usage();
+  }
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.setExtraHTTPHeaders({
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+  });
+
+  // TODO: save our cookies from past runs and skip logging in if they're still valid
+  await login(page, "https://runalyze.com/login");
+  const cookies = await page.cookies();
+  const cookieString = cookies
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join("; ");
+  await browser.close();
+
+  await getActivities(cookieString);
+}
+
+await main();
---
+++ parseActivities.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+import fs from "node:fs";
+import { inspect } from "node:util";
+import { parse } from "node-html-parser";
+
+const METERS_PER_MILE = 1609.344;
+const METERS_PER_FOOT = 0.3048;
+
+// normalize all numbers:
+// - floats to floats
+// - miles, ft or kms to meters
+// - times to seconds
+function tryNumeric(n) {
+  // we use the fact that parseFloat throws away the end of strings, so
+  // "77 %" -> 77
+  if (n.match(/^[\d\.]+$/)) return parseFloat(n);
+  if (n.match(/^[\d\.]+\s+%$/)) return parseFloat(n) / 100;
+  if (n.match(/^[\d\.]+\s+mi$/)) return parseFloat(n) * METERS_PER_MILE;
+  if (n.match(/^[\d\.]+\s+km$/)) return parseFloat(n) * 1000;
+  if (n.match(/^[\d\.]+\s+ft$/)) return parseFloat(n) * METERS_PER_FOOT;
+
+  // match 34:56 or 2:34:56, and convert to seconds
+  const time = n.match(/^(\d+):(\d+)$/);
+  if (time) return parseFloat(time[1]) * 60 + parseFloat(time[2]);
+  const time2 = n.match(/^(\d+):(\d+):(\d+)$/);
+  if (time2)
+    return (
+      parseFloat(time2[0]) * 3600 +
+      parseFloat(time2[1]) * 60 +
+      parseFloat(time2[2])
+    );
+
+  // otherwise, we didn't find a format we knew, so just leave it as is
+  return n;
+}
+
+// convert an array of keys and an array of values to an object
+function dict(keys, values) {
+  return Object.assign(...keys.map((k, i) => ({ [k]: values[i] })));
+}
+
+export function parseActivities(body) {
+  const root = parse(body);
+  const table = root.querySelector("table");
+
+  // parse each header. Use the text if present, otherwise the title attribute,
+  // as its value
+  const headers = table
+    .querySelectorAll("thead td span")
+    .map((td) => td.textContent.trim() || td.attrs["title"]);
+
+  // pull all rows out of the table that have an activity in them
+  const rows = table
+    // there are two <tbody>s, the first is the one wejwant; the second contains
+    // summary info
+    .querySelector("tbody")
+    .querySelectorAll("tr")
+    .filter((x) => x.querySelectorAll("td").length > 3);
+
+  // for each row in the table, get its values and normalize their units
+  const values = rows.map((x) =>
+    x
+      .querySelectorAll("td")
+      // filter out the submenu td, which doesn't have a corresponding header
+      .filter((td) => !td.querySelector("div.inline-menu"))
+      .map(
+        (td) =>
+          // TODO: normalize distances to meters and times to seconds
+          tryNumeric(td.textContent.trim()) ||
+          td.querySelector("i")?.attrs["class"],
+      )
+      .slice(1),
+  );
+
+  // if there are multiple activities on a single day, the date isn't listed in
+  // each row. Grab it from the previous row that had a date
+  for (let i = 0; i < values.length; i++) {
+    if (values[i].length == headers.length - 1) {
+      values[i].splice(
+        0,
+        0,
+        values.slice(i).findLast((val) => val[0].includes("/")),
+      );
+    }
+  }
+
+  // validate that each row has the same # of values as the header list
+  if (values.filter((v) => v.length != headers.length).length > 0) {
+    throw new Error(
+      `invalid row found: ${inspect(
+        values.filter((v) => v.length != headers.length),
+      )}`,
+    );
+  }
+
+  return values.map((v) => dict(headers, v));
+}
